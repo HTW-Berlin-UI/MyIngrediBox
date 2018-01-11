50c413c66ed9397f9649245f52de19894a089c66
kkoal <k.koal@student.htw-berlin.de>
2018-01-11 18:11:54 +0100

    Add backbone for json import testing

--- src/myIngrediBox/agents/inventoryManager/InventoryManagerAgent.java
+++ src/myIngrediBox/agents/inventoryManager/InventoryManagerAgent.java
@@ -1,6 +1,8 @@
 package myIngrediBox.agents.inventoryManager;
 
 import jade.core.Agent;
+import jade.core.behaviours.SequentialBehaviour;
+import myIngrediBox.shared.behaviours.ReadIngredientsFromFile;
 
 public class InventoryManagerAgent extends Agent {
 
@@ -9,4 +11,16 @@ public class InventoryManagerAgent extends Agent {
 	 */
 	private static final long serialVersionUID = 1L;
 
+	protected void setup() {
+		super.setup();
+
+		ReadIngredientsFromFile loadInventory = new ReadIngredientsFromFile("path");
+		SequentialBehaviour manageInventory = new SequentialBehaviour();
+
+		manageInventory.addSubBehaviour(loadInventory);
+
+		loadInventory.setDataStore(manageInventory.getDataStore());
+
+		this.addBehaviour(manageInventory);
+	}
 }
