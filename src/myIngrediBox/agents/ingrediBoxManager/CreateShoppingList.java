558be3ee431e3d3a398dee705734b78a24a4352e
kkoal <k.koal@student.htw-berlin.de>
2018-02-10 17:37:33 +0100

    Add Leftover calculation

--- src/myIngrediBox/agents/ingrediBoxManager/CreateShoppingList.java
+++ src/myIngrediBox/agents/ingrediBoxManager/CreateShoppingList.java
@@ -3,83 +3,82 @@ package myIngrediBox.agents.ingrediBoxManager;
 import java.util.ArrayList;
 import java.util.Iterator;
 
-import jade.core.Agent;
 import jade.core.behaviours.OneShotBehaviour;
 import myIngrediBox.ontologies.Ingredient;
 
 public class CreateShoppingList extends OneShotBehaviour {
 
-    private static final long serialVersionUID = 1L;
+	private static final long serialVersionUID = 1L;
 
+	public CreateShoppingList() {
+		super();
+	}
 
-    public CreateShoppingList() {
-	super();
-    }
+	// Creates shoppingList, which contains ingredients unavailable from inventory
+	@Override
+	public void action() {
 
-    
-    // Creates shoppingList, which contains ingredients unavailable from inventory
-    @Override
-    public void action() {
+		ArrayList<Ingredient> availableIngredientList = (ArrayList<Ingredient>) this.getDataStore()
+				.get("availableIngredients");
+		ArrayList<Ingredient> shoppingList = new ArrayList<>();
+		ArrayList<Ingredient> recipe = (ArrayList<Ingredient>) this.getDataStore().get("recipe");
+		ArrayList<Ingredient> requiredIngredients = new ArrayList<Ingredient>(recipe);
 
-	ArrayList<Ingredient> availableIngredientList = (ArrayList<Ingredient>) this.getDataStore().get("availableIngredients");
-	ArrayList<Ingredient> shoppingList = new ArrayList<>();
-	ArrayList<Ingredient> recipe = (ArrayList<Ingredient>) this.getDataStore().get("recipe");
+		Iterator<Ingredient> availableIngredientIterator = availableIngredientList.iterator();
+		Iterator<Ingredient> recipeIterator = requiredIngredients.iterator();
 
-	Iterator<Ingredient> availableIngredientIterator = availableIngredientList.iterator();
-	Iterator<Ingredient> recipeIterator = recipe.iterator();
+		if (!availableIngredientList.isEmpty()) {
+			// Check order necessity
+			while (recipeIterator.hasNext()) {
+				Ingredient recipeIngredient = (Ingredient) recipeIterator.next();
 
-	if (!availableIngredientList.isEmpty()) {
-	    // Check order necessity
-	    while (recipeIterator.hasNext()) {
-		Ingredient recipeIngredient = (Ingredient) recipeIterator.next();
+				if (availableIngredientIterator.hasNext()) {
+					Ingredient availableIngredient = (Ingredient) availableIngredientIterator.next();
 
-		if (availableIngredientIterator.hasNext()) {
-		    Ingredient availableIngredient = (Ingredient) availableIngredientIterator.next();
+					if (availableIngredientList.contains(recipeIngredient)) {
 
-		    if (availableIngredientList.contains(recipeIngredient)) {
+						int indexRp = requiredIngredients.indexOf(availableIngredient);
 
-			int indexRp = recipe.indexOf(availableIngredient);
+						boolean haveSameUnit = recipeIngredient.getUnit().equals(availableIngredient.getUnit());
 
-			boolean haveSameUnit = recipeIngredient.getUnit().equals(availableIngredient.getUnit());
+						// Set remaining quantity of request and inventory ingredient
+						if (haveSameUnit) {
+							if (recipeIngredient.getQuantity() > availableIngredient.getQuantity()) {
+								requiredIngredients.get(indexRp).setQuantity(
+										recipeIngredient.getQuantity() - availableIngredient.getQuantity());
+								shoppingList.add(recipeIngredient);
+							}
+						}
+					}
+				}
 
-			// Set remaining quantity of request and inventory ingredient
-			if (haveSameUnit) {
-			    if (recipeIngredient.getQuantity() > availableIngredient.getQuantity()) {
-				recipe.get(indexRp).setQuantity(
-					recipeIngredient.getQuantity() - availableIngredient.getQuantity());
-				shoppingList.add(recipeIngredient);
-			    }
+				// Add ingredi's to shoppinglist, which weren't available from inventory
+				else {
+					shoppingList.add(recipeIngredient);
+				}
 			}
-		    }
 		}
+		// Set ShoppingList
+		this.getDataStore().put("shoppingList", shoppingList);
 
-		// Add ingredi's to shoppinglist, which weren't available from inventory
-		else {
-		    shoppingList.add(recipeIngredient);
+		// Printing available ingredients received
+		availableIngredientIterator = availableIngredientList.iterator();
+		System.out.println("\nIBM received available ingredients: ");
+		while (availableIngredientIterator.hasNext()) {
+			Ingredient ingredient = availableIngredientIterator.next();
+			System.out.print(ingredient.getQuantity() + " " + ingredient.getName() + "\t");
 		}
-	    }
-	}
-	// Set ShoppingList
-	this.getDataStore().put("shoppingList", shoppingList);
+		System.out.println("\n");
 
-	// Printing available ingredients received
-	availableIngredientIterator = availableIngredientList.iterator();
-	System.out.println("\nIBM received available ingredients: ");
-	while (availableIngredientIterator.hasNext()) {
-	    Ingredient ingredient = availableIngredientIterator.next();
-	    System.out.print(ingredient.getQuantity() + " " + ingredient.getName() + "\t");
-	}
-	System.out.println("\n");
+		// Print ingredients, remaining on shoppinglist
+		System.out.println("\nThis ingredients remain on shoppinglist after checking inventory: ");
+		Iterator<Ingredient> shoppingListIterator = shoppingList.iterator();
+		while (shoppingListIterator.hasNext()) {
+			Ingredient ingredient = shoppingListIterator.next();
+			System.out.print(ingredient.getQuantity() + " " + ingredient.getName() + "\t");
+		}
+		System.out.println("\n");
 
-	// Print ingredients, remaining on shoppinglist
-	System.out.println("\nThis ingredients remain on shoppinglist after checking inventory: ");
-	Iterator<Ingredient> shoppingListIterator = shoppingList.iterator();
-	while (shoppingListIterator.hasNext()) {
-	    Ingredient ingredient = shoppingListIterator.next();
-	    System.out.print(ingredient.getQuantity() + " " + ingredient.getName() + "\t");
 	}
-	System.out.println("\n");
-
-    }
 
 }
--- src/myIngrediBox/agents/ingrediBoxManager/IngrediBoxManagerAgent.java
+++ src/myIngrediBox/agents/ingrediBoxManager/IngrediBoxManagerAgent.java
@@ -14,91 +14,94 @@ import myIngrediBox.shared.behaviours.ReadFromFile;
 
 public class IngrediBoxManagerAgent extends Agent {
 
-    private static final long serialVersionUID = 1L;
+	private static final long serialVersionUID = 1L;
 
-    @Override
-    protected void setup() {
-	super.setup();
+	@Override
+	protected void setup() {
+		super.setup();
 
-	// Initialize behaviour to manage recipe
-	SequentialBehaviour manageRecipe = new SequentialBehaviour();
+		// Initialize behaviour to manage recipe
+		SequentialBehaviour manageRecipe = new SequentialBehaviour();
 
-	this.getContentManager().registerLanguage(new SLCodec());
-	this.getContentManager().registerOntology(IngrediBoxOntology.getInstance());
+		this.getContentManager().registerLanguage(new SLCodec());
+		this.getContentManager().registerOntology(IngrediBoxOntology.getInstance());
 
-	// Load Recipe
-	ReadFromFile loadRecipe = new ReadFromFile("assets/recipes/EierkuchenSpezial.json");
-	ParseRecipe parseRecipe = new ParseRecipe();
-	PrintRecipeIngredientList printRecipeIngredientBehaviour = new PrintRecipeIngredientList();
+		// Load Recipe
+		ReadFromFile loadRecipe = new ReadFromFile("assets/recipes/EierkuchenSpezial.json");
+		ParseRecipe parseRecipe = new ParseRecipe();
+		PrintRecipeIngredientList printRecipeIngredientBehaviour = new PrintRecipeIngredientList();
 
-	loadRecipe.setDataStore(manageRecipe.getDataStore());
-	parseRecipe.setDataStore(manageRecipe.getDataStore());
-	printRecipeIngredientBehaviour.setDataStore(manageRecipe.getDataStore());
+		loadRecipe.setDataStore(manageRecipe.getDataStore());
+		parseRecipe.setDataStore(manageRecipe.getDataStore());
+		printRecipeIngredientBehaviour.setDataStore(manageRecipe.getDataStore());
 
-	manageRecipe.addSubBehaviour(loadRecipe);
-	manageRecipe.addSubBehaviour(parseRecipe);
-	manageRecipe.addSubBehaviour(printRecipeIngredientBehaviour);
+		manageRecipe.addSubBehaviour(loadRecipe);
+		manageRecipe.addSubBehaviour(parseRecipe);
+		manageRecipe.addSubBehaviour(printRecipeIngredientBehaviour);
 
-	// IMB-IM-Communication
+		// IMB-IM-Communication
 
-	SequentialBehaviour findInventoryThanCheckAvailability = new SequentialBehaviour();
-	findInventoryThanCheckAvailability.setDataStore(manageRecipe.getDataStore());
+		SequentialBehaviour findInventoryThanCheckAvailability = new SequentialBehaviour();
+		findInventoryThanCheckAvailability.setDataStore(manageRecipe.getDataStore());
 
-	DFQueryBehaviour findInventory = new DFQueryBehaviour(this, "Inventory-Managing-Service",
-		manageRecipe.getDataStore());
+		DFQueryBehaviour findInventory = new DFQueryBehaviour(this, "Inventory-Managing-Service",
+				manageRecipe.getDataStore());
 
-	findInventoryThanCheckAvailability.addSubBehaviour(findInventory);
+		findInventoryThanCheckAvailability.addSubBehaviour(findInventory);
 
-	// Check availability in inventory
-	ACLMessage im = new ACLMessage(ACLMessage.REQUEST);
-	im.setConversationId("inventory-request");
-	InventoryRequest inventoryRequest = new InventoryRequest(this, im,
-		manageRecipe.getDataStore());
-	inventoryRequest.setDataStore(manageRecipe.getDataStore());
-	findInventoryThanCheckAvailability.addSubBehaviour(inventoryRequest);
+		// Check availability in inventory
+		ACLMessage im = new ACLMessage(ACLMessage.REQUEST);
+		im.setConversationId("inventory-request");
+		InventoryRequest inventoryRequest = new InventoryRequest(this, im, manageRecipe.getDataStore());
+		inventoryRequest.setDataStore(manageRecipe.getDataStore());
+		findInventoryThanCheckAvailability.addSubBehaviour(inventoryRequest);
 
-	manageRecipe.addSubBehaviour(findInventoryThanCheckAvailability);
+		manageRecipe.addSubBehaviour(findInventoryThanCheckAvailability);
 
-	// IMB-IM-Communication end
+		// IMB-IM-Communication end
 
-	// Create shopping list
-	CreateShoppingList createShoppingList = new CreateShoppingList();
-	createShoppingList.setDataStore(manageRecipe.getDataStore());
-	manageRecipe.addSubBehaviour(createShoppingList);
+		// Create shopping list
+		CreateShoppingList createShoppingList = new CreateShoppingList();
+		createShoppingList.setDataStore(manageRecipe.getDataStore());
+		manageRecipe.addSubBehaviour(createShoppingList);
 
-	// IBM-IB-Communication
+		// IBM-IB-Communication
 
-	SequentialBehaviour findBuyerThanBuy = new SequentialBehaviour();
-	findBuyerThanBuy.setDataStore(manageRecipe.getDataStore());
-	DFQueryBehaviour findBuyer = new DFQueryBehaviour(this, "Ingredient-Buying-Service",
-		manageRecipe.getDataStore());
+		SequentialBehaviour findBuyerThanBuy = new SequentialBehaviour();
+		findBuyerThanBuy.setDataStore(manageRecipe.getDataStore());
+		DFQueryBehaviour findBuyer = new DFQueryBehaviour(this, "Ingredient-Buying-Service",
+				manageRecipe.getDataStore());
 
-	ACLMessage m = new ACLMessage(ACLMessage.REQUEST);
-	m.setProtocol(FIPANames.InteractionProtocol.FIPA_REQUEST);
-	m.setConversationId("buyer-request");
-	BuyerRequest buy = new BuyerRequest(this, m, manageRecipe.getDataStore());
+		ACLMessage m = new ACLMessage(ACLMessage.REQUEST);
+		m.setProtocol(FIPANames.InteractionProtocol.FIPA_REQUEST);
+		m.setConversationId("buyer-request");
+		BuyerRequest buy = new BuyerRequest(this, m, manageRecipe.getDataStore());
 
-	findBuyerThanBuy.addSubBehaviour(findBuyer);
-	findBuyerThanBuy.addSubBehaviour(buy);
+		findBuyerThanBuy.addSubBehaviour(findBuyer);
+		findBuyerThanBuy.addSubBehaviour(buy);
 
-	manageRecipe.addSubBehaviour(findBuyerThanBuy);
-	// IBM-IB-Communication end
+		manageRecipe.addSubBehaviour(findBuyerThanBuy);
+		// IBM-IB-Communication end
 
-	this.addBehaviour(new WakerBehaviour(this, 200) {
+		// Calculate leftovers and send them to inventory
+		CalculateLeftovers calculateLeftovers = new CalculateLeftovers(manageRecipe.getDataStore());
+		manageRecipe.addSubBehaviour(calculateLeftovers);
 
-	    protected void onWake() {
-		this.getAgent().addBehaviour(manageRecipe);
-	    }
+		this.addBehaviour(new WakerBehaviour(this, 200) {
 
-	});
-    }
+			protected void onWake() {
+				this.getAgent().addBehaviour(manageRecipe);
+			}
 
-    @Override
-    protected void takeDown()
+		});
+	}
 
-    {
-	this.addBehaviour(new DeregisterServiceBehaviour(this));
-	super.takeDown();
-    }
+	@Override
+	protected void takeDown()
+
+	{
+		this.addBehaviour(new DeregisterServiceBehaviour(this));
+		super.takeDown();
+	}
 
 }
